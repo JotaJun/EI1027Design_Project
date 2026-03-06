ee6844f7a753e261095eeaf7ee13b563238c5496
Juan-crm-uji <al436992@uji.es>
2026-03-06 11:32:45 +0100

    Creación proyecto

---
+++ SgOviProject/src/main/java/es/uji/ei1027/SgOviProject/SgOviProjectApplication.java
@@ -0,0 +1,13 @@
+package es.uji.ei1027.SgOviProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgOviProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgOviProjectApplication.class, args);
+	}
+
+}
---
+++ SgOviProject/src/test/java/es/uji/ei1027/SgOviProject/SgOviProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uji.ei1027.SgOviProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgOviProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
