20e1c9987585bdef838f3a75d55e32cc03376fbd
Juan-crm-uji <al436992@uji.es>
2026-03-06 12:03:15 +0100

    Cambios en dependencias

--- SgOviProject/src/main/java/es/uji/ei1027/SgOviProject/SgOviProjectApplication.java
+++ SgOviProject/src/main/java/es/uji/ei1027/SgOviProject/SgOviProjectApplication.java
@@ -1,10 +1,21 @@
 package es.uji.ei1027.SgOviProject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class SgOviProjectApplication {
+
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SgOviProjectApplication.class, args);
