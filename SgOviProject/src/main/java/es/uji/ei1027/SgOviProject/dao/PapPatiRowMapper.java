84c2d0e5eb42c4951560a2d84866850b1595f997
Juan-crm-uji <al436992@uji.es>
2026-03-11 13:20:27 +0100

    Implementación Dao Pap/Pati

--- SgOviProject/src/main/java/es/uji/ei1027/SgOviProject/dao/PapPatiRowMapper.java
+++ SgOviProject/src/main/java/es/uji/ei1027/SgOviProject/dao/PapPatiRowMapper.java
@@ -1,4 +1,29 @@
 package es.uji.ei1027.SgOviProject.dao;
 
-public class PapPatiRowMapper {
+import es.uji.ei1027.SgOviProject.enums.StaffType;
+import es.uji.ei1027.SgOviProject.model.PapPati;
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public final class PapPatiRowMapper implements RowMapper<PapPati> {
+
+    public PapPati mapRow(ResultSet rs, int rowNum) throws SQLException {
+        PapPati papPati = new PapPati();
+        papPati.setDni(rs.getString("dni"));
+
+        String typeStr = rs.getString("stafftype");
+        if (typeStr != null) {
+            papPati.setStaffType(StaffType.valueOf(typeStr));
+        }
+
+        papPati.setInitialAvailableDate(rs.getObject("initialAvailableDate", LocalDate.class));
+        papPati.setLastAvailableDate(rs.getObject("lastAvailableDate", LocalDate.class));
+        papPati.setTraining(rs.getString("training"));
+        papPati.setYearsExperience(rs.getInt("yearsExperience"));
+        papPati.setUrlCv(rs.getString("urlCv"));
+
+        return papPati;
+    }
 }
