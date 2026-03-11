cc1b0ac58592105ef9b3c5421051f6f27e550b7d
Juan-crm-uji <al436992@uji.es>
2026-03-11 11:44:02 +0100

    Preparacion daos de: Account, PapPati y AssistanceRequest

---
+++ SgOviProject/src/main/java/es/uji/ei1027/SgOviProject/model/Account.java
@@ -0,0 +1,14 @@
+package es.uji.ei1027.SgOviProject.model;
+
+
+import java.time.LocalDate;
+
+public class Account {
+    private String dni;
+    private String name;
+    private String surname;
+    private LocalDate birthday;
+    private String password;
+    private String email;
+    Private
+}
