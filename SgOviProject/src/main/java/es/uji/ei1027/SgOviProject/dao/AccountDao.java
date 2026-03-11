fe9af031b9e4fb8d0475ad3f414311abe508a1e5
Juan-crm-uji <al436992@uji.es>
2026-03-11 12:07:35 +0100

    Implementado Dao Account

--- SgOviProject/src/main/java/es/uji/ei1027/SgOviProject/dao/AccountDao.java
+++ SgOviProject/src/main/java/es/uji/ei1027/SgOviProject/dao/AccountDao.java
@@ -1,4 +1,74 @@
 package es.uji.ei1027.SgOviProject.dao;
 
+import es.uji.ei1027.SgOviProject.model.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class AccountDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void addAccount(Account account) {
+        jdbcTemplate.update("INSERT INTO Account(dni, name, surname, birthday, password, email, phoneNumber, city, street, zipCode, gender) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                account.getDni(), account.getName(), account.getSurname(), account.getBirthday(),
+                account.getPassword(), account.getEmail(), account.getPhoneNumber(),
+                account.getCity(), account.getStreet(), account.getZipCode(), account.getGender());
+    }
+
+    public void deleteAccount(String dni) {
+        jdbcTemplate.update("DELETE FROM Account WHERE dni=?", dni);
+    }
+
+    public void deleteAccount(Account account) {
+        jdbcTemplate.update("DELETE FROM Account WHERE dni=?", account.getDni());
+    }
+
+    // Actualiza to_do MENOS dni
+    public void updateAccount(Account account) {
+        jdbcTemplate.update("UPDATE Account SET name=?, surname=?, birthday=?, password=?, email=?, phoneNumber=?, city=?, street=?, zipCode=?, gender=? WHERE dni=?",
+                account.getName(), account.getSurname(), account.getBirthday(),
+                account.getPassword(), account.getEmail(), account.getPhoneNumber(),
+                account.getCity(), account.getStreet(), account.getZipCode(), account.getGender(),
+                account.getDni());
+    }
+
+    public Account getAccount(String dni) {
+        try {
+            return jdbcTemplate.queryForObject("SELECT * FROM Account WHERE dni=?",
+                    new AccountRowMapper(), dni);
+        } catch(EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    // Aprovechamos que es clave alternativa
+    public Account getAccountByEmail(String email) {
+        try {
+            return jdbcTemplate.queryForObject("SELECT * FROM Account WHERE email=?",
+                    new AccountRowMapper(), email);
+        } catch(EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    public List<Account> getAccounts() {
+        try {
+            return jdbcTemplate.query("SELECT * FROM Account",
+                    new AccountRowMapper());
+        } catch (EmptyResultDataAccessException e) {
+            return new ArrayList<Account>();
+        }
+    }
 }
