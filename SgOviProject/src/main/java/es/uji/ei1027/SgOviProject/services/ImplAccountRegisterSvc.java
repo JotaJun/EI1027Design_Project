1fc534e6554e649e7b92c45fdfd4651246ee0fbe
Juan-crm-uji <al436992@uji.es>
2026-03-11 15:37:01 +0100

    Servicio para agregar OviUser y PapPati
    Muy importantes para que no haya problemas al agregar a la vez un PapPati con su respectiva cuenta.

---
+++ SgOviProject/src/main/java/es/uji/ei1027/SgOviProject/services/ImplAccountRegisterSvc.java
@@ -0,0 +1,44 @@
+package es.uji.ei1027.SgOviProject.services;
+
+import es.uji.ei1027.SgOviProject.dao.AccountDao;
+import es.uji.ei1027.SgOviProject.dao.PapPatiDao;
+import es.uji.ei1027.SgOviProject.model.Account;
+import es.uji.ei1027.SgOviProject.model.PapPati;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional  // Mirar documentación Spring lo que significa
+public class ImplAccountRegisterSvc implements  IntAccountRegisterSvc{
+
+    @Autowired
+    AccountDao accountDao;
+
+    @Autowired
+    PapPatiDao papPatiDao;
+
+    @Autowired
+    OviUserDao oviUserDao;
+
+
+    @Override
+    public void addPapPati(Account account, PapPati papPati) {
+        accountDao.addAccount(account);
+
+        // Si papPati no tiene el dni, se lo asignamos
+        papPati.setDni(account.getDni());
+
+        papPatiDao.addPapPati(papPati);
+    }
+
+    @Override
+    public void addOviUser(Account account, OviUser oviUser) {
+        accountDao.addAccount(account);
+
+        // Si oviUser no tiene el dni, se lo asignamos
+        oviUser.setDni(account.getDni());
+
+        oviUserDao.addOviUser(oviUser);
+    }
+}
