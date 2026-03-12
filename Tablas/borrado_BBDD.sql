1142c22468f2d52cfa44b62c839f5ff3e12d2499
Juan-crm-uji <al436992@uji.es>
2026-03-12 15:35:26 +0100

    script_borrado_BBDD

---
+++ Tablas/borrado_BBDD.sql
@@ -0,0 +1,17 @@
+-- 1. Tablas de tercer nivel
+DROP TABLE IF EXISTS ActivityAssistance;
+DROP TABLE IF EXISTS Communication;
+DROP TABLE IF EXISTS Contract;
+
+-- 2. Tablas de segundo nivel
+DROP TABLE IF EXISTS Candidacy;
+DROP TABLE IF EXISTS Activity;
+
+-- 3. Tablas de primer nivel
+DROP TABLE IF EXISTS AssistanceRequest;
+DROP TABLE IF EXISTS Instructor;
+DROP TABLE IF EXISTS OviUser;
+DROP TABLE IF EXISTS PapPati;
+
+-- 4. Tabla base
+DROP TABLE IF EXISTS Account;
