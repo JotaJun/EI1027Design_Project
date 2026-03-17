63bf43c8b3edb141dbf0f59dad55429a37377d6e
Juan-crm-uji <al436992@uji.es>
2026-03-17 13:00:39 +0100

    cambios insert pap pati

--- Tablas/inserts_pappati.sql
+++ Tablas/inserts_pappati.sql
@@ -1,7 +1,7 @@
-INSERT INTO PapPati (dni, stafftype, initialAvailableDate, lastAvailableDate, training, yearsOfExperience, urlCv) VALUES
-('40404040D', 'PAP',  '2024-01-01', '2024-12-31', 'Grado en Ciencias del Deporte', 3, 'https://mi-cv.es/mtorres'),
-('50505050E', 'PATI', NULL,         NULL,         'Grado en Psicología', 5, 'https://mi-cv.es/screspo'),
-('60606060F', 'PAP',  '2024-01-01', '2024-12-31', 'TAFAD', 2, 'https://mi-cv.es/icastro'),
-('70707070G', 'PATI', '2024-01-01', '2024-12-31', 'Grado en Educación Primaria', 8, 'https://mi-cv.es/jnavarro'),
-('80808080K', 'PAP',  NULL,         NULL,         'Magisterio EF', 10, 'https://mi-cv.es/ghernandez'),
-('90909090W', 'PATI', '2024-01-01', '2024-12-31', 'Pedagogía Terapéutica', 1, 'https://mi-cv.es/fruiz');
+INSERT INTO PapPati (dni, stafftype, drivingLicense, initialAvailableDate, lastAvailableDate, training, yearsOfExperience, urlCv) VALUES
+('40404040D', 'PAP',  TRUE,  '2024-01-01', '2024-12-31', 'Grado en Ciencias del Deporte', 3, 'https://mi-cv.es/mtorres'),
+('50505050E', 'PATI', FALSE, NULL,         NULL,         'Grado en Psicología', 5, 'https://mi-cv.es/screspo'),
+('60606060F', 'PAP',  TRUE,  '2024-01-01', '2024-12-31', 'TAFAD', 2, 'https://mi-cv.es/icastro'),
+('70707070G', 'PATI', TRUE,  '2024-01-01', '2024-12-31', 'Grado en Educación Primaria', 8, 'https://mi-cv.es/jnavarro'),
+('80808080K', 'PAP',  FALSE, NULL,         NULL,         'Magisterio EF', 10, 'https://mi-cv.es/ghernandez'),
+('90909090W', 'PATI', TRUE,  '2024-01-01', '2024-12-31', 'Pedagogía Terapéutica', 1, 'https://mi-cv.es/fruiz');
