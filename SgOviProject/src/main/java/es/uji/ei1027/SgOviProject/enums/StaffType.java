8bb88b5c9189df5a052f5f0f26cca42ce35ea3aa
Juan-crm-uji <al436992@uji.es>
2026-05-05 13:19:02 +0200

    mejorado enum stafftype

--- SgOviProject/src/main/java/es/uji/ei1027/SgOviProject/enums/StaffType.java
+++ SgOviProject/src/main/java/es/uji/ei1027/SgOviProject/enums/StaffType.java
@@ -1,6 +1,16 @@
 package es.uji.ei1027.SgOviProject.enums;
 
 public enum StaffType {
-    PAP,
-    PATI
+    PAP("Professional d'Assistència Personal"),
+    PATI("Professional d'Assistència Terapèutica Infantil");
+
+    private final String description;
+
+    StaffType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
