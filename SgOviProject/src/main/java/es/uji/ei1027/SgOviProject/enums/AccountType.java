9c91d27541cf9ba8329113692745dd781a93cc56
Juan-crm-uji <al436992@uji.es>
2026-05-05 13:24:56 +0200

    AccountType mejorado

--- SgOviProject/src/main/java/es/uji/ei1027/SgOviProject/enums/AccountType.java
+++ SgOviProject/src/main/java/es/uji/ei1027/SgOviProject/enums/AccountType.java
@@ -1,8 +1,18 @@
 package es.uji.ei1027.SgOviProject.enums;
 
 public enum AccountType {
-    OVIUSER,
-    PAPPATI,
-    LEGALGUARDIAN,
-    TECHNICIAN
+    OVIUSER("Persona usuària de l'OVI"),
+    PAPPATI("Professional d'assistència (PAP/PATI)"),
+    LEGALGUARDIAN("Tutor legal o garant"),
+    TECHNICIAN("Personal tècnic de l'OVI");
+
+    private final String description;
+
+    AccountType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
